fe27d07c3a446f74e4ddae3eb7784b0df6767732
DESKTOP-PHKB03R\han <1156781974@qq.com>
2019-02-26 21:17:44 +0800

    初始化项目运行插件

---
+++ cmake-build-debug/CMakeFiles/LearnCPP.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "E:/CPPWorkSpace/LearnCPP/Learn/Chapter4/ArrayTest.cpp" "E:/CPPWorkSpace/LearnCPP/cmake-build-debug/CMakeFiles/LearnCPP.dir/Learn/Chapter4/ArrayTest.cpp.obj"
+  "E:/CPPWorkSpace/LearnCPP/Learn/Chapter4/ConstPointTest.cpp" "E:/CPPWorkSpace/LearnCPP/cmake-build-debug/CMakeFiles/LearnCPP.dir/Learn/Chapter4/ConstPointTest.cpp.obj"
+  "E:/CPPWorkSpace/LearnCPP/Learn/Chapter4/PointTest.cpp" "E:/CPPWorkSpace/LearnCPP/cmake-build-debug/CMakeFiles/LearnCPP.dir/Learn/Chapter4/PointTest.cpp.obj"
+  "E:/CPPWorkSpace/LearnCPP/Learn/Chapter4/StructTest.cpp" "E:/CPPWorkSpace/LearnCPP/cmake-build-debug/CMakeFiles/LearnCPP.dir/Learn/Chapter4/StructTest.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/LearnCPP.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LearnCPP.dir/Learn/Chapter4/ArrayTest.cpp.obj"
+  "CMakeFiles/LearnCPP.dir/Learn/Chapter4/StructTest.cpp.obj"
+  "CMakeFiles/LearnCPP.dir/Learn/Chapter4/PointTest.cpp.obj"
+  "CMakeFiles/LearnCPP.dir/Learn/Chapter4/ConstPointTest.cpp.obj"
+  "LearnCPP.pdb"
+  "LearnCPP.exe"
+  "LearnCPP.exe.manifest"
+  "libLearnCPP.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LearnCPP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
