6f85362b2a28545d86f53a613f17e2a33f95f44a
DESKTOP-PHKB03R\han <1156781974@qq.com>
2019-02-27 23:22:53 +0800

    no message

--- Learn/Chapter4/ConstPointTest.cpp
+++ Learn/Chapter4/ConstPointTest.cpp
@@ -2,7 +2,8 @@
 // Created by han on 2019/2/24.
 //
 #include <iostream>
-
+#include <vector>
+#include <array>
 int
 main(){
     using namespace std;
@@ -18,20 +19,37 @@ main(){
    //ptr2[1]='2';
    //std::cout<<"=====================;";
    //std::cout<<ptr2;
-    int array1[4]={1,2,3};
+   // int array1[4]={1,2,3};
+//
+   // int *array2=new int[4]{1,2,4,3};
+   // std::cout<<sizeof array1<<std::endl;
+   // std::cout<<sizeof array2<<endl;
+   // char * p1="1234";
+   // cout<<p1[2]<<endl;
+   // char char1[20]="bear";
+   // char *char2=new char[strlen(char1)+1];
+   // cout<<"char2的长度是"<<strlen(char1)<<endl;
+   // strcpy(char2,char1);
+   // cout<<char2<<endl;
+   // 
+   // int int1[4]{1,2,3,4};
+   // cout<< sizeof(int1);
+   //vector动态数组
+    vector<double> a2(4);
+    a2[0]=1.0/3.0;
+    a2[1]=1.0/3.0;
+    a2[2]=1.0/3.0;
+    a2[3]=1.0/3.0;
+    a2.at(2)=5;
+    cout<<"hhhh"<<endl;
+    cout<<a2[0]<<" | "<<&a2[0]<<endl;
+    cout<<a2[2]<<" | "<<&a2[2]<<endl;
 
-    int *array2=new int[4]{1,2,4,3};
-    std::cout<<sizeof array1<<std::endl;
-    std::cout<<sizeof array2<<endl;
-    char * p1="1234";
-    cout<<p1[2]<<endl;
-    char char1[20]="bear";
-    char *char2=new char[strlen(char1)+1];
-    cout<<"char2的长度是"<<strlen(char1)<<endl;
-    strcpy(char2,char1);
-    cout<<char2<<endl;
-    
-    int int1[4]{1,2,3,4};
-    cout<< sizeof(int1);
+    array<int,5> a3;
+    a3.at(1)=6;
+    cout<<a3.at(-1)<<" | "<<&a2[2]<<endl;
+
+    cout<<"hhhh";
+
     return 0;
 }
